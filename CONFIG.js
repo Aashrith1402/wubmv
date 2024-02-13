831e3d24441715eab19de6d6df56ab03e3319604
Aashrith1402 <100113404+Aashrith1402@users.noreply.github.com>
2024-02-14 02:31:03 +0530

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIGDATA = {
+    titleHeader: "WILL YOU BE MY VALENTINE",
+    descriptionHeader: "If you exit and don’t answer, it means you have AGREED already. ^^",
+    buttonYes: "YESSS",
+    buttonNo: "NO,MAYBE NEXT TIME",
+    titleModar: "THANK YOU FOR ACCEPTING",
+    descriptionModar: 'You have made my day so special and memorable by accepting my proposal.^^ </br> I am overjoyed and grateful to have you in my life. You are the most wonderful person I have ever met :)) </br>  and I cannot wait to share more moments with you. Thank you for being my valentine, and for making me feel so loved and cherished. </br> You are my dream come true, and I hope to make you happy every day"',
+};
